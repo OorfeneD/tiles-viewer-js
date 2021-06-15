8b89beb5566b1b933ed47ed3179296d882367f8d
OorfeneDeuce <oorfened@gmail.com>
2021-06-15 12:16:09 +0000

    prettier changes

--- src/main.js
+++ src/main.js
@@ -12,26 +12,28 @@
  */
 const fillArray = (length, foo) => {
   const arr = new Array(length)
-  for(let i = 0; i < length; i ++) {
+  for (let i = 0; i < length; i++) {
     arr.push(foo(i))
   }
   return arr
 }
 
-const position = {x: 0, y: 0, scale: 1}
-const size = {width: 0, height: 0}
+const position = { x: 0, y: 0, scale: 1 }
+const size = { width: 0, height: 0 }
 const scales = fillArray(10, (i) => Math.pow(2, 10 + i))
 /** 
  * Create canvas with TilesViewer
  * @param {string} elementSelector - selector of element for rendering TilesViewer inside
- * @param {(ImageProperies | string)} imageProperties - object of type {@link ImageProperies} or string with link to file with information about image, to be rendered
+ * @param {(ImageProperies | string)} imageProperties - object of type {@link ImageProperties} or string with link to file with information about image, to be rendered
  * @returns {boolean} is creating was finished success
 */
 function initTilesViewer(elementSelector, imageProperties) {
   const canvas = document.createElement('canvas')
   const ctx = canvas.getContext('2d')
   const width, height = (canvas.width, canvas.height)
-  
+  size.width = imageProperties.width
+  size.height = imageProperties.height
+
 }
 
 /**
@@ -178,4 +180,6 @@ class TilesViewer {
     return { left, right, top, bottom }
   }
 }
+
 export default initTilesViewer
+export default TilesViewer
